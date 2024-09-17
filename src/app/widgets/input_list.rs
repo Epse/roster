e6817af0978cb777bc48da46ac4fa95ba038e7b4
Stef Pletinck <stef.pletinck@ugent.be>
2024-09-17 13:19:51 +0200

    Remove unnecessary log

--- src/app/widgets/input_list.rs
+++ src/app/widgets/input_list.rs
@@ -20,7 +20,6 @@ pub fn input_list(data: &mut Vec<String>, ui: &mut egui::Ui, title: &str) {
                 .unwrap_or(String::from(""));
             let input_id = key.with("tmp_input");
             let input = ui.add(egui::TextEdit::singleline(&mut text).id(input_id).desired_width(f32::INFINITY));
-            println!("{:?}", &input.id);
             if ui.button("+").clicked()
                 || (input.lost_focus() && input.ctx.input(|i| i.key_pressed(egui::Key::Enter)))
             {
